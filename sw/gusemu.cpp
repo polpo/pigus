ac6e584d1253178f11c07ebb61384687f5bd5a86
Ian Scott <ian@occipital.com>
2022-06-09 22:10:31 -0600

    Clean up

--- sw/gusemu.cpp
+++ sw/gusemu.cpp
@@ -67,20 +67,18 @@ void GusEmu::HandleIOWInterrupt(void *pParam)
     io_port_t port = ((gpios >> 12) & 0x3FF) - GUS_PORT_BASE;
     io_val_t value;
     switch (port) {
-        case 0x302:
-        case 0x303:
-        case 0x304: // 3x4 supports 16-bit transfers but PiGUS doesn't! force byte
-        case 0x305:
+        case 0x200:
         case 0x208:
         case 0x209:
-        case 0x307:
-        // Board Only
-        case 0x200:
         case 0x20b:
+        case 0x302:
+        case 0x303:
+        case 0x304:
+        case 0x305:
+        case 0x307:
             value = (gpios >> 4) & 0xFF;
             // let's a go
-            pThis->gus->WriteToPort(port + GUS_PORT_BASE, value, io_width_t::byte);
-            /* CActLED::Get()->Blink(1); */
+            pThis->gus->WriteToPort(port + GUS_PORT_BASE, value, io_width_t::byte); // 3x4 supports 16-bit transfers but PiGUS doesn't! force byte
             break;
     }
 }
@@ -100,48 +98,18 @@ void GusEmu::HandleIORInterrupt(void *pParam)
     /* pThis->m_Logger.Write("GusEmu", LogNotice, "IOR port %d", port); */
     uint8_t value;
     switch (port) {
+        case 0x206:
+        case 0x208:
+        case 0x20a:
         case 0x302:
         case 0x303:
         case 0x304:
         case 0x305:
-        case 0x206:
-        case 0x208:
-        // Board Only
-        case 0x20a:
         case 0x307:
-            if (!(gpios & 0x2)) {
-                // falling edge - read data
-                /*
-                for (size_t i = 0; i < 8; ++i) {
-                    pThis->m_DataPins[i]->SetMode(GPIOModeOutput);
-                }
-                */
-                value = pThis->gus->ReadFromPort(port + GUS_PORT_BASE, io_width_t::byte);
-                /* value = 0x55; */
-                /* CGPIOPin::WriteAll(value << 4, 0xFF0); */
-                FastGPIO::FastGPIOWriteData(value, TRUE);
-                /* value = 0xAA; */
-                /* CGPIOPin::WriteAll(value << 4, 0xFF0); */
-                /* FastGPIO::FastGPIOWriteData(value); */
-                /* CActLED::Get()->On(); */
-                /*
-                CTimer::SimpleusDelay(1);
-                */
-                pThis->m_Timer.nsDelay(1500);
-                FastGPIO::FastGPIOClear();
-            } else {
-                // rising edge - Set data pins back to inputs
-                /*
-                for (unsigned i=4; i < 12; ++i) {
-                    CGPIOPin pin(i, GPIOModeInput);
-                }
-                for (size_t i = 0; i < 8; ++i) {
-                    pThis->m_DataPins[i]->SetMode(GPIOModeInput);
-                }
-                */
-                /* FastGPIO::FastGPIOClear(); */
-                /* CActLED::Get()->Off(); */
-            }
+            value = pThis->gus->ReadFromPort(port + GUS_PORT_BASE, io_width_t::byte);
+            FastGPIO::FastGPIOWriteData(value, TRUE);
+            pThis->m_Timer.nsDelay(1500);
+            FastGPIO::FastGPIOClear();
             break;
     }
 }
@@ -151,14 +119,14 @@ void GusEmu::HandleIORInterrupt(void *pParam)
 void GusEmu::IOTask(void) {
     m_Logger.Write("GusEmu", LogNotice, "IOTask starting up (overridden)");
 
-    u8 curr_iow, last_iow = 1, curr_ior, last_ior = 1;
+    u8 curr_iow, last_iow = 0x1, curr_ior, last_ior = 0x2;
 
     for (;;) {
 	/* gpios = CGPIOPin::ReadAll(); */
         gpios = FastGPIO::FastGPIORead();
 	curr_iow = gpios & 0x1;
 #ifndef USE_HYBRID_POLLING
-	curr_ior = (gpios & 0x2) >> 0x1;
+	curr_ior = gpios & 0x2;
 #endif
 
         u16 port = ((gpios >> 12) & 0x3FF) - GUS_PORT_BASE;
@@ -172,7 +140,7 @@ void GusEmu::IOTask(void) {
 	}
 #ifndef USE_HYBRID_POLLING
 	// Be careful of race conditions here -- should this be an else if?
-	if (curr_ior != last_ior) {  // rising falling edge of ~IOR
+	if (curr_ior < last_ior) {  // falling edge of ~IOR
             HandleIORInterrupt(this);
 	    /* CMultiCoreSupport::SendIPI(3, IPI_IOR); */
 	}
